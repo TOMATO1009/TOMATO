ddbc8ae37c904812c759dc3def9072fbbdd44a43
TOMATO1009 <130633657+TOMATO1009@users.noreply.github.com>
2023-06-14 04:42:27 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
  
 });
+const navbarToggle = document.querySelector('.navbar__toggleBtn');
+const contentbox2 = document.querySelector('.contentbox2');
+
+navbarToggle.addEventListener('click', function () {
+  contentbox2.classList.toggle('open');
+});
